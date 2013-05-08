37a72db98db960b9da894406e9f0648e6c168d56
EmanuelCadems <emanuel_cadems@hotmail.com>
2013-05-08 18:05:40 -0300

    [First Commit: Installing gem spree]

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyStore::Application.config.secret_token = '3af21f056dc90b36a9bef1e45a0da57f228952a2ff566e6f676099e82022b77a7ae1a3691942514f278fba7838bf6906231b86b78fb2bb836a13826e040d45cb'
