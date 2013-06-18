b125eeb7a78a4a159762895c3f9eed083658164c
EmanuelCadems <emanuel_cadems@hotmail.com>
2013-06-18 18:22:26 -0300

    [Issue #1: Adding config s3 to spree]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,12 +9,4 @@ MyStore::Application.configure do
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
 
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
 end
